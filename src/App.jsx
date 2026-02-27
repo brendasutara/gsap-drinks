8b5eb81f50e0277579a2bfae40f0307fbadcf228
Brenda Sutara <sutarabrendagmail.com>
2026-02-26 22:12:43 -0300

    hero section ui y animacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-import React from "react";
+import { ScrollTrigger } from "gsap/all";
 import Navbar from "./components/Navbar";
+import HeroSection from "./sections/HeroSection";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
   return (
     <main>
       <Navbar />
+      <HeroSection />
+      <div className="h-dvh border border-purple-400" />
     </main>
   );
 };
