03faaed08519da988ce263d6b7e055caeb42661f
Brenda Sutara <sutarabrendagmail.com>
2026-03-03 15:13:15 -0300

    Flavor slider

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import { ScrollTrigger } from "gsap/all";
+import { ScrollSmoother, ScrollTrigger } from "gsap/all";
 import Navbar from "./components/Navbar";
 import HeroSection from "./sections/HeroSection";
 import gsap from "gsap";
 import MessageSection from "./sections/MessageSection";
+import FlavorSection from "./sections/FlavorSection";
+import { useGSAP } from "@gsap/react";
 
-gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 const App = () => {
+  useGSAP(() => {
+    ScrollSmoother.create({
+      smooth: 1.5,
+      effects: true,
+    });
+  });
   return (
     <main>
       <Navbar />
-      <HeroSection />
-      <MessageSection />
-      <div className="h-screen border border-pink-500"></div>
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <HeroSection />
+          <MessageSection />
+          <FlavorSection />
+          <div className="h-screen border border-pink-500"></div>
+        </div>
+      </div>
     </main>
   );
 };
