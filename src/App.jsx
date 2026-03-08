2b20d5d7f439dc7a591dcaeb7ae8a427abd05b17
Brenda Sutara <sutarabrendagmail.com>
2026-03-08 13:28:55 -0300

    VideoPinSection

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import MessageSection from "./sections/MessageSection";
 import FlavorSection from "./sections/FlavorSection";
 import { useGSAP } from "@gsap/react";
 import { NutritionSection } from "./sections/NutritionSection";
+import BenefitSection from "./sections/BenefitSection";
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
@@ -25,6 +26,7 @@ const App = () => {
           <MessageSection />
           <FlavorSection />
           <NutritionSection />
+          <BenefitSection />
           <div className="h-screen border border-pink-500"></div>
         </div>
       </div>
